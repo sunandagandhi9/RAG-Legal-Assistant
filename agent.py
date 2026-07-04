310c88c0d88f8884baabd174e866f1f2a1b458d9
sunandagandhi9 <91276007+sunandagandhi9@users.noreply.github.com>
2026-07-04 14:26:03 +1000

    Replace OpenAI APIs with local LLM and HuggingFace embeddings

--- agent.py
+++ agent.py
@@ -1,15 +1,16 @@
 import utils as Utils
 import os as OS
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
+
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from langchain_chroma import Chroma
+from langchain.chains import create_history_aware_retriever
+from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain_core.chat_history import BaseChatMessageHistory
 
 class NewsChat:
@@ -19,11 +20,15 @@ class NewsChat:
 
     def __init__(self, article_id: str):
         oai_key = OS.getenv("OPENAI_API_KEY")
-        embeddings = OpenAIEmbeddings(openai_api_key=oai_key)
+        embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
         self.session_id = article_id
         
         # llm = ChatOpenAI(openai_api_key=oai_key)
-        llm = ChatOpenAI(openai_api_key=oai_key, model='gpt-4o')
+        llm = ChatOllama(
+    model="llama3"
+)
         db = Chroma(persist_directory=Utils.DB_FOLDER, embedding_function=embeddings, collection_name='collection_1')
         retriever = db.as_retriever()
 
